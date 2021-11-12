cb7dfccc50509c804a94e4aad250db2ed98fd3cb
WillyS <willyavellaneda2021@gmail.com>
2021-11-12 10:22:51 -0300

    [Simulador Suma de Dinero y Simulador IVA]

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ nombre = "Guille";
 var edad;
 edad = 41;
 console.log(nombre + edad);
+//sumar valores
+function sumar(valor1, valor2) {
+    return valor1 + valor2;
+}
+console.log(sumar(2, 8));
+//calculador iva  (colocando en precio distintos montos, se obtiene por consola el monto final con iva incluido)
+var precio;
+precio = 400;
+var iva;
+iva = 1.21;
+console.log(precio * iva);
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-//primer console log+
+//primer console log
 console.log("Trabajando en typescript")
 
 //variable
@@ -7,3 +7,18 @@ nombre= "Guille";
 var edad;
 edad= 41;
 console.log(nombre + edad);
+
+//Sumando valores (colocando de entrada dos valores, se obtiene la suma total de los mismos)
+function sumar (valor1: number, valor2:number){
+    return valor1 + valor2;
+}
+console.log(sumar(2,8));
+
+//Simulador Calculador IVA  (colocando de entrada un monto, se obtiene por consola el monto final con Iva incluido)
+var precio;
+precio = 400;
+var iva;
+iva=1.21;
+console.log(precio*iva)
+
+
