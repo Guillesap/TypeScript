7ce1a26132063f218a75b1042a0b301dd7460083
WillyS <willyavellaneda2021@gmail.com>
2021-11-12 09:29:48 -0300

    [primera variable]

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+//primer console log+
 console.log("Trabajando en typescript");
+//variable
+var nombre;
+nombre = "Guille";
+var edad;
+edad = 41;
+console.log(nombre + edad);
--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
+//primer console log+
 console.log("Trabajando en typescript")
+
+//variable
+var nombre;
+nombre= "Guille";
+var edad;
+edad= 41;
+console.log(nombre + edad);
